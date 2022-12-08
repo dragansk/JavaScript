a9c61cb813669def1ded3ef66b98b6d7d3e4b4d4
BojanStojchevski <108979808+BojanStojchevski@users.noreply.github.com>
2022-12-08 22:51:44 +0100

    Add files via upload

---
+++ class2/script.js
@@ -0,0 +1,24 @@
+let userInput = prompt("Type your birthyear")
+let parsedInput = parseInt(userInput)
+
+let chineseZodiac = (parsedInput - 4) % 12
+console.log(chineseZodiac)
+console.log(typeof(chineseZodiac))
+
+
+
+if(!Number.isNaN(chineseZodiac))
+
+
+if( chineseZodiac = 0){
+     alert("Your sing is Rat")
+
+} else if( chineseZodiac = 1){
+    alert("Your sign is Ox")
+
+} esle if ()
+
+
+
+
+else
